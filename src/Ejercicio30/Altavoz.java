f38c6e884bf0bd7327cf443ca97b6fbfa9d8c4d5
JuanMedina05 <juanperezmedina2005@gmail.com>
2025-02-11 12:21:23 +0100

    Añadir ejercicios 30

---
+++ src/Ejercicio30/Altavoz.java
@@ -0,0 +1,32 @@
+package Ejercicio30;
+
+public class Altavoz {
+    public static final int VOL_MAX = 255;
+    public static final int VOL_MIN = 0;
+    private int volumen;
+    public Altavoz(){
+        this.volumen = 0;
+    }
+    public void ponerVolumenMaximo(){
+        this.volumen = VOL_MAX;
+    }
+    public void setVolumen(int v){
+        if(v>=VOL_MIN&&v<=VOL_MAX){
+            this.volumen = v;
+        }else{
+            throw new IllegalArgumentException("Volumen fuera de rango");
+        }
+    }
+    public int getVolumen(){
+        return this.volumen;
+    }
+    public String toString(){
+        int porcentaje = (int)Math.round(this.volumen*10.0/255);
+        String result = "["+this.volumen+"]";
+        for(int i=0;i<10;i++){
+            result += porcentaje>0 ? "*":"-";
+            porcentaje--;
+        }
+        return result;
+    }
+}
---
+++ src/Ejercicio30/test/AltavozTest.java
@@ -0,0 +1,29 @@
+package Ejercicio30.test;
+
+import Ejercicio30.Altavoz;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AltavozTest {
+    @Test
+    public void Test1(){
+        Altavoz a = new Altavoz();
+        assertEquals(Altavoz.VOL_MIN,a.getVolumen());
+        a.ponerVolumenMaximo();
+        assertEquals(Altavoz.VOL_MAX,a.getVolumen());
+    }
+    @Test(expected=java.lang.IllegalArgumentException.class)
+    public void Test2(){
+        Altavoz a = new Altavoz();
+        a.setVolumen(-28);
+    }
+    @Test
+    public void Test3(){
+        Altavoz a = new Altavoz();
+        a.setVolumen(127);
+        System.out.println(a.toString());
+        assertEquals("[127]*****-----",a.toString());
+    }
+
+}
