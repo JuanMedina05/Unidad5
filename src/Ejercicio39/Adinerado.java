c64aab4b259bcfc83cd4b921a176e17b1a7a8912
JuanMedina05 <juanperezmedina2005@gmail.com>
2025-02-17 08:30:14 +0100

    Añadir clase ejercicio 39

---
+++ src/Ejercicio39/Adinerado.java
@@ -0,0 +1,7 @@
+package Ejercicio39;
+
+public interface Adinerado {
+    public abstract double getDineroTotal();
+    public abstract boolean añadirDinero(int cantidad);
+    public abstract boolean retirarDinero(int cantidad);
+}
